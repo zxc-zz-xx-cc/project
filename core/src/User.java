1ce0758d4bbb7fbcf5ed9916d8f94b5bf16a0c93
zzz <771272908@qq.com>
2020-11-25 16:46:49 +0800

    word转PDF

---
+++ core/src/User.java
@@ -0,0 +1,30 @@
+import sun.font.FontRunIterator;
+
+import java.util.HashMap;
+
+/**
+ * @author zxc
+ * @date 2020/11/23 - 11:08
+ */
+public class User {
+    public static void main() {
+        aaa();
+        String[] arr = new String[]{"a", "b", "c"};
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("name", "Tom");
+        map.put("age", "19");
+        if (arr == null) {
+
+        }
+        if (arr != null) {
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+    }
+    private static void aaa() {
+        System.out.println("123");
+    }
+    public static final String abc = "abc";
+}
---
+++ core/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hy
+  Date: 2020/11/23
+  Time: 16:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>标题</title>
+  </head>
+  <body>
+    <h1>我的页面</h1>
+  </body>
+</html>
